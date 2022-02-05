876834d491bd7e3e6c7c508f089865dd5e76fe87
muchaoxu <1522928438@qq.com>
2022-02-05 16:00:33 +0800

    三级分类组件化

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,13 +1,23 @@
 // home组件的仓库
 import {reqCategoryList} from '@/api'
 
-const state = {}
-const mutations = {}
+const state = {
+  // 数据默认值不能乱写，要看返回数据的类型
+  categoryList: [],
+}
+const mutations = {
+  CATEGORYLIST(state, categoryList){
+    state.categoryList = categoryList;
+  }
+}
 const actions = {
   // 通过 API 里面的接口调用，向服务器发请求，获取服务器的数据
-  async categoryList(){
+  async categoryList(context){
     let result = await reqCategoryList();
-    console.log(result);
+    if(result.code == 200){
+      // 不能直接写 commit，要用上下文(context)
+      context.commit("CATEGORYLIST", result.data);
+    }
   }
 }
 const getters = {}
