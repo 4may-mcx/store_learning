c02d515f1742166d30184fc5a1c68d844dce08b0
muchaoxu <1522928438@qq.com>
2022-02-05 10:49:37 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import router from '@/router'
+import TypeNav from "@/pages/Home/TypeNav"
 
 Vue.config.productionTip = false
+Vue.component(TypeNav.name, TypeNav)
 
 new Vue({
   render: h => h(App),
